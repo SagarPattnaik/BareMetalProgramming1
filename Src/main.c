5cf377072deb8e4a1ed50d0941010f483d6eafa2
pattnasa2013 <sagarpattnaik60@gmail.com>
2023-06-03 23:18:23 -0400

    Hello World using SWV

--- Src/main.c
+++ Src/main.c
@@ -16,14 +16,15 @@
  ******************************************************************************
  */
 
-#include <stdint.h>
-
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+#include<stdio.h>
+
 int main(void)
 {
     /* Loop forever */
+	printf("Hello world\n");
 	for(;;);
 }
