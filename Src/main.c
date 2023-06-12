ed5b2a18435a4f21b0bce5b23f3a2ca738d10deb
pattnasa2013 <sagarpattnaik60@gmail.com>
2023-06-12 16:02:21 -0400

    Change MSP to PSP and Try an Exception

--- Src/main.c
+++ Src/main.c
@@ -31,6 +31,12 @@ void generate_interrupt()
 /* This function executes in THREAD MODE of the processor */
 int main(void)
 {
+  int control_reg = 0x02;
+  uint32_t psp_value = 0x20008000;
+
+  __asm volatile ("msr PSP, %0"::"r"(psp_value));
+  __asm volatile ("msr CONTROL, %0"::"r"(control_reg));
+  
 	printf("In thread mode : before interrupt\n");
 
 	generate_interrupt();
