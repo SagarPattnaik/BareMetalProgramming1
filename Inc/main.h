be410d870ebbe761ea5be45b1b322f317499c3f7
pattnasa2013 <sagarpattnaik60@gmail.com>
2023-06-17 14:23:04 -0400

    Creating private STACK for tasks

---
+++ Inc/main.h
@@ -0,0 +1,26 @@
+/*
+ * main.h
+ *
+ *  Created on: Jun 17, 2023
+ *      Author: LENOVO
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define MAX_TASKS   5
+/* some stack memory calculations */
+#define SIZE_TASK_STACK          1024U
+#define SIZE_SCHED_STACK         1024U
+
+#define SRAM_START               0x20000000U
+#define SIZE_SRAM                ( (128) * (1024))
+#define SRAM_END                 ((SRAM_START) + (SIZE_SRAM) )
+
+#define T1_STACK_START           SRAM_END
+#define T2_STACK_START           ( (SRAM_END) - (1 * SIZE_TASK_STACK) )
+#define T3_STACK_START           ( (SRAM_END) - (2 * SIZE_TASK_STACK) )
+#define T4_STACK_START           ( (SRAM_END) - (3 * SIZE_TASK_STACK) )
+#define IDLE_STACK_START         ( (SRAM_END) - (4 * SIZE_TASK_STACK) )
+#define SCHED_STACK_START        ( (SRAM_END) - (5 * SIZE_TASK_STACK) )
+#endif /* MAIN_H_ */
